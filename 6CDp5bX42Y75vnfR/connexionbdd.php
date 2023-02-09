f2dd63c6c8b226479db17d74fb59869c2779c9da
Arthur <80512736+ArthurBergere@users.noreply.github.com>
2023-02-09 13:55:30 +0100

    Update connexionbdd.php

--- 6CDp5bX42Y75vnfR/connexionbdd.php
+++ 6CDp5bX42Y75vnfR/connexionbdd.php
@@ -2,7 +2,7 @@
 
 try
 {
-    $pdo = new PDO('mysql:host=localhost:3306;dbname=kaminstr_prod', 'kaminstr_Admin', 'Kam!45instrumentale');
+    $pdo = new PDO('mysql:host=localhost:3306;dbname=kaminstr_prod', 'kaminstr_Admin', 'mdp');
     $pdo ->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     $pdo ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
